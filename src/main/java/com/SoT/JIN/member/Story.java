cd55724988585a7a769ebb1eff304c96a8594c4f
JIN <tjdghksne@g.eulji.ac.kr>
2024-07-03 12:36:01 +0900

    최근 업로드 기능 구현(Story 엔티티에 작성일자 추가, MemberController에서 최근 업로드 일수 계산 메서드 추가

--- src/main/java/com/SoT/JIN/member/Story.java
+++ src/main/java/com/SoT/JIN/member/Story.java
@@ -1,10 +1,10 @@
 package com.SoT.JIN.member;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,9 +13,7 @@ import java.util.Set;
 @Setter
 public class Story {
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long storyId;
     private String username;
     private String image_url;
@@ -25,9 +23,20 @@ public class Story {
     private String tags;
     private String description;
     private int viewCount;
+
+    // 업로드 시간을 저장할 필드
+    private LocalDateTime uploadTime;
+
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL)
     private Set<LikeEntity> likes = new HashSet<>();
+
     public int getLikesCount() {
         return this.likes.size();
+    }
+
+    // 엔터티가 영속화되기 전에 호출되는 메서드
+    @PrePersist
+    protected void onCreate() {
+        this.uploadTime = LocalDateTime.now();
     }
 }

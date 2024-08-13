dd135844c20d54713949cf46ba716d9c7a719ee3
6s-hwan <tjdghksne@g.eulji.ac.kr>
2024-08-13 10:21:39 +0900

    following.css : 보기 편하게 수정(임시) StoryRepository : 사용자별 게시글 수를 세는 메서드 추가 UserRepository : 팔로우한 순서대로 보여주는 쿼리문 추가 following.html : 네비게이션 바 추가, thymeleaf 적용, 팔로잉 페이지 구현 : 팔로잉 하는 사람들의 정보( username, profileImage, totalStories), 작가 페이지 이동 경로 추가 FollowsController : /follows 매핑 추가 : 로그인한 사용자의 팔로잉 리스트를 가져옴(역순) 팔로잉 하는 사람들의 정보를 모델에 추가

--- src/main/java/com/SoT/JIN/user/UserRepository.java
+++ src/main/java/com/SoT/JIN/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.SoT.JIN.user;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
@@ -12,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPhoneNumberAndVerificationCode(String phoneNumber, String verificationCode);
 
+    @Query("SELECT u FROM User u JOIN u.followers f WHERE f.userId = :userId")
+    List<User> findFollowingByUserId(@Param("userId") Long userId);
 }

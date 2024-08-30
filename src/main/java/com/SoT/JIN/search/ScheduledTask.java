dc4c3fd17add2f50469172b2bbd2d9b17edfcab8
6s-hwan <tjdghksne@g.eulji.ac.kr>
2024-08-31 08:15:00 +0900

    ScheduledTask :매주 월요일 초기화 메서드 합침 Home.js : 디버깅용 콘솔 추가

--- src/main/java/com/SoT/JIN/search/ScheduledTask.java
+++ src/main/java/com/SoT/JIN/search/ScheduledTask.java
@@ -16,17 +16,16 @@ public class ScheduledTask {
         this.risingService = risingService;
         this.searchService = searchService;
     }
-
-    // 매주 월요일 00:00시에 실행되는 스케줄러
+    // 매주 월요일 00:00시에 Search 및 Rising 데이터를 초기화하고 업데이트하는 스케줄러
     @Scheduled(cron = "0 0 0 * * MON")
-    public void resetRisingKeywordsWeekly() {
+    public void resetAndUpdateWeekly() {
+        // 1. 검색 횟수 초기화
         searchService.resetSearchCounts();  // 1등부터 8등까지 검색 횟수 초기화
-    }
 
-    // 매주 월요일 00:00시에 Rising 데이터를 초기화하고 업데이트하는 스케줄러
-    @Scheduled(cron = "0 0 0 * * MON")
-    public void resetRisingWeekly() {
+        // 2. Rising 데이터를 초기화
         risingService.resetRisingData(); // 모든 Rising 데이터를 초기화
-        risingService.updateRisingFromSearch(searchService); // Rising 데이터를 SearchService 기반으로 업데이트
+
+        // 3. Rising 데이터를 SearchService 기반으로 업데이트
+        risingService.updateRisingFromSearch(searchService); // SearchService 기반으로 Rising 데이터 업데이트
     }
 }
